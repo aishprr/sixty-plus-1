32da263237c48512072875f4b1193cf7d087f164
Aishwarya Prem Renu <apremrenu@gmail.com>
2016-09-06 14:32:07 -0400

    resources automatic rendering

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    
+  	# Reading all then names and descriptions of resources
+  	@resources = Resource.limit(Resource.count())
   end
-
+  
 end
